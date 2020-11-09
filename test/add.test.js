6d726c9b5f0867826329598ddffbed6be6225249
Vesa Koskela <koskela.vesatuomas@gmail.com>
2020-11-09 14:11:57 +0200

    Implemented tests for add.js

--- test/add.test.js
+++ test/add.test.js
@@ -1,3 +1,17 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import add from "../src/add.js"
+
+test('Positive integers', () => {
+  expect(add(1,1)).toBe(2)
+})
+
+test('Postive and negative integer', () => {
+  expect(add(1,-1).toBe(0))
+})
+
+test('Negative integers', () => {
+  expect(add(-1,-1).toBe(-2))
+})
+
+test('Floating points', () => {
+  expect(add(1.1, 1.1)).toEqual(2.2)
 })
