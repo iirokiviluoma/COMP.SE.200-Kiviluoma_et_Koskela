9b616110c2a03164b766ed1f998cd355e68fc252
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-12-06 21:25:56 +0200

    Tests for toInteger & toNumber.

--- test/toInteger.test.js
+++ test/toInteger.test.js
@@ -1,3 +1,14 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import toInteger from '../src/toInteger'
+
+test('Min possible number returns 0', () => {
+  expect(toInteger(Number.MIN_VALUE)).toBe(0)
+})
+
+test('Inifinite number returns max possible value', () => {
+  expect(toInteger(Infinity)).toBe(Number.MAX_VALUE)
+})
+
+test('Odd positive number rounds it down', () => {
+  const odd = 1.5
+  expect(toInteger(odd)).toBe(Math.floor(odd))
 })
--- test/toNumber.test.js
+++ test/toNumber.test.js
@@ -1,3 +1,48 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import toNumber from '../src/toNumber'
+
+import isObject from '../src/isObject.js'
+
+test('Number returns itself', () => {
+  const num = 2020.12
+  expect(toNumber(num)).toBe(num)
+})
+
+test('Number string converts it to number', () => {
+  const str = '2020'
+  expect(toNumber(str)).toBe(Number(str))
+})
+
+test('Symbol from number returns NaN', () => {
+  const sym = Symbol(2020)
+  expect(toNumber(sym)).toBe(NaN)
+})
+
+test('Object returns NaN', () => {
+  const obj = {
+    key1: 1,
+    key2: 2
+  }
+
+  expect(toNumber(obj)).toBe(NaN)
+})
+
+test('Null returns 0', () => {
+  expect(toNumber(null)).toBe(0)
+})
+
+test('Object with valueOf-function returns its value', () => {
+  const func = () => 2020
+  
+  const obj = {
+    valueOf: func
+  }
+
+  expect(toNumber(obj)).toBe(2020)
+})
+
+test('Object with valueOf-string returns NaN', () => {
+  const obj = {
+    valueOf: 'test'
+  }
+  expect(toNumber(obj)).toBe(NaN)
 })
