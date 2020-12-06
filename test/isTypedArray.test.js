bc1ca96cd8bf957d1f9bc5731bc59d1ccd89b0d2
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-12-06 17:19:03 +0200

    Test for isTypedArray & keys.

--- test/isTypedArray.test.js
+++ test/isTypedArray.test.js
@@ -1,3 +1,24 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import isTypedArray from '../src/isTypedArray'
+
+test('Empty array returns false', () => {
+  expect(isTypedArray([])).toBe(false)
+})
+
+test('Uint array returns true', () => {
+  const arr = new Uint8Array(2)
+  expect(isTypedArray(arr)).toBe(true)
+})
+
+test('Float array returns true', () => {
+  const arr = new Float32Array(2)
+  expect(isTypedArray(arr)).toBe(true)
+})
+
+test('Null returns false', () => {
+  expect(isTypedArray(null)).toBe(false)
+})
+
+test('Object returns false', () => {
+  const obj = { key: 'value' }
+  expect(isTypedArray(obj)).toBe(false)
 })
