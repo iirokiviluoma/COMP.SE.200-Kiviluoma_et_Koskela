dbbc57174c9c6d5ea7c681cbf3c414b0c9c4b930
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-12-06 17:59:13 +0200

    Test for map, reduce & slice (slice still needs one more test).

--- test/map.test.js
+++ test/map.test.js
@@ -1,3 +1,18 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import map from '../src/map'
+
+test('Integer array and function with one param', () => {
+  const arr = [2, 4, 6]
+  const func = x => 2*x
+  expect(map(arr, func)).toEqual([4, 8, 12])
+})
+
+test('Array and null returning function returns nulls', () => {
+  const arr = [1, 2, 3]
+  const func = () => null
+  expect(map(arr, func)).toEqual([null, null, null])
+})
+
+test('Null returns empty array', () => {
+  const func = () => null
+  expect(map(null, func)).toEqual([])
 })
--- test/slice.test.js
+++ test/slice.test.js
@@ -1,3 +1,36 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import slice from '../src/slice'
+
+test('Array without start and end', () => {
+  const arr = [1, 2, 3, 4, 5]
+  expect(slice(arr)).toEqual(arr)
+})
+
+test('Array with negative start', () => {
+  const arr = [1, 2, 3, 4, 5]
+  expect(slice(arr, -1)).toEqual([5])
+})
+
+test('Array with negative end', () => {
+  const arr = [1, 2, 3, 4, 5]
+  expect(slice(arr, 0, -1)).toEqual([1, 2, 3, 4])
+})
+
+test('Array with end > Array.length', () => {
+  const arr = [1, 2, 3, 4, 5]
+  const ans = [].concat(arr)
+  ans.push(undefined)
+  expect(slice(arr, 0, arr.length + 1)).toEqual(ans)
+})
+
+test('Array with start > end', () => {
+  const arr = [1, 2, 3, 4, 5]
+  expect(slice(arr, 2, 1)).toEqual([])
+})
+
+test('Empty array returns empty array', () => {
+  expect(slice([])).toEqual([])
+})
+
+test('Null returns empty array', () => {
+  expect(slice(null)).toEqual([])
 })
