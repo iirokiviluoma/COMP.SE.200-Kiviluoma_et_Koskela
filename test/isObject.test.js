8fee5a7213cf39c2939615112e0045a9afeb2699
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-12-06 16:57:10 +0200

    Test for isObject, isObjectLike & isSymbol.

--- test/isObject.test.js
+++ test/isObject.test.js
@@ -1,3 +1,33 @@
-test('Dummy test', () => {
+import isObject from '../src/isObject'
+
+test('Array returns true', () => {
   expect(1).toBe(1)
+})
+
+test('Object with some keys returns true', () => {
+  const obj = { key: 'value' }
+  expect(isObject(obj)).toBe(true)
+})
+
+test('Object without keys returns true', () => {
+  expect(isObject({})).toBe(true)
+})
+
+test('Null returns false', () => {
+  expect(isObject(null)).toBe(false)
+})
+
+test('Function returns true', () => {
+  const func = () => null
+  expect(isObject(func)).toBe(true)
+})
+
+test('String returns false', () => {
+  const str = 'test'
+  expect(isObject(str)).toBe(false)
+})
+
+test('Number returns false', () => {
+  const num = 0
+  expect(isObject(num)).toBe(false)
 })
--- test/isSymbol.test.js
+++ test/isSymbol.test.js
@@ -1,3 +1,25 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import isSymbol from '../src/isSymbol'
+
+test('Symbol returns true', () => {
+  const sym = Symbol()
+  expect(isSymbol(sym)).toBe(true)
+})
+
+test('Symbol inside object returns true', () => {
+  const objSym = new Object(Symbol('test'))
+  expect(isSymbol(objSym)).toBe(true)
+})
+
+test('Null returns false', () => {
+  expect(isSymbol(null)).toBe(false)
+})
+
+test('Number returns false', () => {
+  const num = 0
+  expect(isSymbol(num)).toBe(false)
+})
+
+test('String returns false', () => {
+  const str = 'test'
+  expect(isSymbol(str)).toBe(false)
 })
