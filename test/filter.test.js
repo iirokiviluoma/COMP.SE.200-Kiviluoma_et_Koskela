a15e19d35ceea6425ed2e761f7d0c3e768eb0903
Vesa Koskela <koskela.vesatuomas@gmail.com>
2020-12-07 20:10:18 +0200

    tests added for 'filter'

--- test/filter.test.js
+++ test/filter.test.js
@@ -1,3 +1,19 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import filter from '../src/filter'
+
+test('One object found', () => {
+  const users = [
+    { 'user': 'barney', 'active': true },
+    { 'user': 'fred',   'active': false }
+  ]
+  
+  expect(filter(users, ({active}) => active)).toStrictEqual([{ 'user': 'barney', 'active': true }])
+})
+
+test('No objects found', () => {
+  const users = [
+    { 'user': 'barney', 'active': false },
+    { 'user': 'fred',   'active': false }
+  ]
+  
+  expect(filter(users, ({active}) => active)).toStrictEqual([[]])
 })
