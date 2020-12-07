4ed4db89d238b0628fe85f5db7cf596af0a5fc51
Vesa Koskela <koskela.vesatuomas@gmail.com>
2020-12-07 17:16:37 +0200

    Fixed tests for 'at' and uploading reports to s3

--- test/at.test.js
+++ test/at.test.js
@@ -1,17 +1,17 @@
 import at from '../src/at'
 
 test('Valid object and one path', () => {
-  expect(at({'a': 2}, 'a[0]')).toBe([2])
+  expect(at({'a': 2}, 'a')).toStrictEqual([2])
 })
 
 test('Valid object and multiple paths', () => {
-  expect(at({'a': 2, 'b': 3}, ['a[0]', 'b[0]'])).toBe([2, 3])
+  expect(at({'a': 2, 'b': [3]}, ['a', 'b[0]'])).toStrictEqual([2, 3])
 })
 
 test('Valid object, invalid path', () => {
-  expect(at({'a': 2}, 'a[1]')).toBe([undefined])
+  expect(at({'a': 2}, 'a[1]')).toStrictEqual([undefined])
 })
 
 test('Valid object, valid and invalid path', () => {
-  expect(at({'a': 2, 'b': 3}, ['a[0]', 'b[1]'])).toBe([2, undefined])
+  expect(at({'a': 2, 'b': 3}, ['a', 'b[1]'])).toStrictEqual([2, undefined])
 })
