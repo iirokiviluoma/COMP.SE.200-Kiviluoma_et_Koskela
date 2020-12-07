0f700d44aad958f9f649d9c6eaeb8540b2aee5e0
Iiro Kiviluoma <iirokiviluoma@outlook.com>
2020-12-07 09:39:50 +0200

    Tests for isDate, upperFirst & words.

--- test/upperFirst.test.js
+++ test/upperFirst.test.js
@@ -1,3 +1,23 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import upperFirst from '../src/upperFirst'
+
+test('Empty string returns empty string', () => {
+  const str = ''
+  expect(upperFirst(str)).toBe((str))
+})
+
+test('All lower case', () => {
+  const str = 'test string'
+  const ans = 'Test string'
+  expect(upperFirst(str)).toBe(ans)
+})
+
+test('All upper case', () => {
+  const str = 'TEST STRING'
+  expect(upperFirst(str)).toBe(str)
+})
+
+test('First word lower, second upper case', () => {
+  const str = 'test STRING'
+  const ans = 'Test STRING'
+  expect(upperFirst(str)).toBe(ans)
 })
