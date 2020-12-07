ea25df9b4309d375f5892cbba4ba830b25f2707a
Vesa Koskela <koskela.vesatuomas@gmail.com>
2020-12-07 16:48:21 +0200

    Implemented 'at' tests and allowed uploading and updating s3 also if build fails.

--- test/at.test.js
+++ test/at.test.js
@@ -1,3 +1,17 @@
-test('Dummy test', () => {
-  expect(1).toBe(1)
+import at from '../src/at'
+
+test('Valid object and one path', () => {
+  expect(at({'a': 2}, 'a[0]')).toBe([2])
+})
+
+test('Valid object and multiple paths', () => {
+  expect(at({'a': 2, 'b': 3}, ['a[0]', 'b[0]'])).toBe([2, 3])
+})
+
+test('Valid object, invalid path', () => {
+  expect(at({'a': 2}, 'a[1]')).toBe([undefined])
+})
+
+test('Valid object, valid and invalid path', () => {
+  expect(at({'a': 2, 'b': 3}, ['a[0]', 'b[1]'])).toBe([2, undefined])
 })
